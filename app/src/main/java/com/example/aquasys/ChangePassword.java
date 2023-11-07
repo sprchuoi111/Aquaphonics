4be5efb50af7406e0cf443cb42ecee2a464cc9e8
sprchuoi <quangbinhlxcity@gmail.com>
2023-11-07 21:06:00 +0700

    update notification v2

---
+++ app/src/main/java/com/example/aquasys/ChangePassword.java
@@ -0,0 +1,16 @@
+package com.example.aquasys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChangePassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password);
+
+
+    }
+}
